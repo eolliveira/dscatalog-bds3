2b9a1ce4e1a4589b83e9f560ee161b69f096f0b0
Erick Oliveira <erick.oliveira9607@gmail.com>
2024-03-24 13:19:57 -0300

    First test

---
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -0,0 +1,12 @@
+import { formatPrice } from 'util/formatters';
+
+test('formatPrice shold format number when given 10.1', () => {
+  //ARRANGE
+  const price = 10.1;
+
+  //ACT
+  const result = formatPrice(price);
+
+  //ASSERT
+  expect(result).toEqual('10,10');
+});
